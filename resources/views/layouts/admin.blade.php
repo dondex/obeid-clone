7ea5c4bc1398c7dfce8fa3eda21307807f848edd
dondex <dex.chavez@yahoo.com>
2025-05-23 22:52:45 +0800

    change meta title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Sun City's Polyclinic & Family Clinic</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Obeid Hospital</title>
+    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- jQuery -->
--- resources/views/layouts/app-nonav.blade.php
+++ resources/views/layouts/app-nonav.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Sun Clinic') }}</title>
+    <title>{{ config('app.name', 'Obeid Hospital') }}</title>
 
     <!-- Fonts -->
     <!-- <link rel="dns-prefetch" href="//fonts.bunny.net">
